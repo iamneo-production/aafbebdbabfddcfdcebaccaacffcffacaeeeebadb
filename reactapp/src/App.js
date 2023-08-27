62b9aa03f6675c725d3ccd4278b9618f9bea85f6
Iamneo <techadmin@iamneo.ai>
2023-08-27 06:12:30 +0000

    Backup Commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import UrlValidator from './components/UrlValidator/UrlValidator';
+function App() {
+  return (
+    <div>
+    <p className="title">Url Validator</p>
+      <UrlValidator />
+    </div>
+  );
+}
+
+export default App;
+
